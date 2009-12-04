bc9ca0f4743a20dc5c730e643ce2424b5c014992
Peter L. Søndergaard <peter@localhost.localdomain>
2009-12-04 12:40:47 +0100

    audtofreq was forgotten, has now been added

---
+++ general/audtofreq.m
@@ -0,0 +1,42 @@
+function freq = audtofreq(scale,aud);
+%AUDTOFREQ  Converts auditory units to frequency (Hz)
+%   Usage: freq = audtofreq(aud);
+%  
+%   AUDTOFREQ(scale,aud) converts values on the selected auditory scale to
+%   values on the frequency scale measured in Hz.
+%
+%   See the help on FREQTOAUD to get a list of the supported values of the
+%   scale parameter. 
+%M
+%   See also: freqtoaud, audspace, audfiltbw
+
+%   AUTHOR: Peter L. Soendergaard
+
+% ------ Checking of input parameters ---------
+
+error(nargchk(2,2,nargin));
+
+if ~isnumeric(aud) ||  all(aud(:)<0)
+  error('%s: aud must be a non-negative number.',upper(mfilename));
+end;
+
+if ~ischar(scale)
+  error('%s: the scale must be denoted by a character string.',upper(mfilename))
+end;
+
+% ------ Computation --------------------------
+  
+switch(lower(scale))
+ case 'mel'
+  freq = 700*(exp(aud/1127.01048)-1);
+ case 'erb'
+  freq = 228.8455*(exp(aud/9.265)-1);
+ case 'bark'
+  % This one was found through http://www.ling.su.se/STAFF/hartmut/bark.htm
+  freq = 1960./(26.81./(aud+0.53)-1);
+ case 'erb83'
+  freq = 14363./(1-exp((aud-43.0)/11.7))-14675;
+ otherwise
+  error(['%s: unknown auditory scale: %s. Please see the help for a list ' ...
+         'of supported scales.'],upper(mfilename),scale);
+end;
