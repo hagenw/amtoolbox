a77e38d5aee1323a326ca99a445fcd26b924febb
Peter L. Soendergaard <soender@users.sourceforge.net>
2009-12-07 12:05:27 +0100

    forgot to add the adaptation loop demo

--- demos/Contents.m
+++ demos/Contents.m
@@ -6,6 +6,7 @@
 %
 %   Psychoacoustics
 %     DEMO_AUDSCALES        - Different auditory scales.
+%     DEMO_ADAPTLOOP        - Adaptation loops.
 %
 %   For help, bug reports, suggestions etc. send email to 
 %   amtoolbox-help@lists.sourceforge.net
