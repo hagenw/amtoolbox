3781693741ea50b7bdddbd4ab290d1e7429a6240
Hagen Wierstorf <hagen.wierstorf@telekom.de>
2009-12-14 15:27:04 +0100

    Deleted the AC component reference in the commentar

--- general/rmsdb.m
+++ general/rmsdb.m
@@ -2,9 +2,9 @@ function y = rmsdb(insig)
 %RMSDB RMS value of signal (in dB)
 %   Usage: y = rmsdb(insig);
 %
-%   RMSDB(insig) computes the RMS (Root Mean Square) value in dB SPL of a finite 
-%   sampled signal sampled at a uniform sampling rate. The Db value is
-%   computed from the convention that 100 Db SPL corresponds to an RMS
+%   RMSDB(insig) computes the RMS (Root Mean Square) value in dB SPL of a 
+%   finite sampled signal sampled at a uniform sampling rate. The dB value is
+%   computed from the convention that 100 dB SPL corresponds to an RMS
 %   value of 1.
 %
 %   See also: rms, gaindb, setleveldb
@@ -21,8 +21,6 @@ end;
 
 % ------ Computation --------------------------
 
-% With var(insig) = rms(insig).^2 - mean(insig).^2 we can 
-% compute the RMS value in dB with 
-% the following formula:
-% y = 20*log10( sqrt(var(insig))*10^5 )
-y = 20*log10( rms(insig))+100;
+% The level of a signal in dB SPL is given by the following formula:
+% level = 20*log10(p/p_0)
+y = 20*log10( rms(insig) )+100;
