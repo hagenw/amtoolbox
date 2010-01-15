ec3c87a90840d4652b468b62561284dda2c6a04e
Peter L. Soendergaard <peter@pspc.elektro.dtu.dk>
2010-01-15 10:06:41 +0100

    added 'hilbert' option to ihcenvelope

--- general/ihcenvelope.m
+++ general/ihcenvelope.m
@@ -12,10 +12,15 @@ function inoutsig = ihcenvelope(inoutsig,fs,methodname)
 %   use. The name refers to a set of papers where in this particular
 %   method has been utilized or studied. The options are
 %
-%-    'dau' - Use a 2nd order butterworth filter with a cut-off frequency of
-%             1000 Hz. This method has been used in all models deriving from
-%             the original 1996 model by Dau et. al. These models are
-%             mostly monaural in nature.
+%-    'dau'     - Use a 2nd order butterworth filter with a cut-off
+%                 frequency of 1000 Hz. This method has been used in all
+%                 models deriving from the original 1996 model by 
+%                 Dau et. al. These models are mostly monaural in nature.
+%
+%-    'hilbert' - Use the Hilbert envelope instead of the half-wave
+%                 rectification and low pass filtering. This is not a
+%                 releastic model of the inner hair envelope extraction
+%                 process, but the option is included for completeness.  
 %
 %R  dau1996qmeI
   
@@ -39,14 +44,18 @@ end;
 
 % ------ Computation -------------------------------------------------
 
-% Half-wave rectification
-inoutsig = max( inoutsig, 0 );
+if ~strcmp(lower(methodname),'hilbert')
+  % Half-wave rectification
+  inoutsig = max( inoutsig, 0 );
+end;
 
 switch(lower(methodname))
  case 'dau'
    cutofffreq=1000;
    [b, a] = butter(2, cutofffreq*2/fs);
    inoutsig = filter(b,a, inoutsig);
+ case 'hilbert'
+  inoutsig = abs(hilbert(inoutsig));
  otherwise
   error('%s: Unknown method name: %s.',upper(mfilename),methodname);
 end;
