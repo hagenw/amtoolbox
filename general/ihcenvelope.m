68c7a5ad8b9827a42bac48a1526d12ff6f838652
Peter L. Soendergaard <peter@pspc.elektro.dtu.dk>
2010-01-20 15:00:06 +0100

    added breebart and bernstein methods in ihcenvelope

--- general/ihcenvelope.m
+++ general/ihcenvelope.m
@@ -12,6 +12,14 @@ function inoutsig = ihcenvelope(inoutsig,fs,methodname)
 %   use. The name refers to a set of papers where in this particular
 %   method has been utilized or studied. The options are
 %
+%-    'bernstein' - Compute the Hilbert envelope, compress the envelope
+%                 by raising it to the power .23, half-wave rectify it, 
+%                 square it and low-pass filter it with a cut-off
+%                 frequency of 425 Hz. This method is defined in Bernstein 1999.
+%
+%-    'breebart' - Use a 5th order filter with a cut-off frequency of 770
+%                 Hz. This method is given in Breebart 2001. Page 94 in thesis.
+%
 %-    'dau'     - Use a 2nd order Butterworth filter with a cut-off
 %                 frequency of 1000 Hz. This method has been used in all
 %                 models deriving from the original 1996 model by 
@@ -24,9 +32,9 @@ function inoutsig = ihcenvelope(inoutsig,fs,methodname)
 %
 %-    'lindemann' - Use a 1st order Butterworth filter with a cut-off frequency
 %                 of 800 Hz. This method is defined in the paper
-%                 Lindemann 1986a. Mostly used for binaural models.
+%                 Lindemann 1986a.
 %
-%R  lindemann1986a dau1996qmeI
+%R  bernstein1999normalized breebaart2001binaural lindemann1986a dau1996qmeI
   
 % FIXME: Which paper did this idea originally appear in?
 
@@ -54,16 +62,30 @@ if ~strcmp(lower(methodname),'hilbert')
 end;
 
 switch(lower(methodname))
+ case 'bernstein'
+  inoutsig=max(abs(hilbert(inoutsig)).^0.23,0).^2;
+  cutofffreq=425;
+  [b, a] = butter(2, cutofffreq*2/fs);
+  inoutsig = filter(b,a, inoutsig);
+ case 'breebart'
+  inoutsig = max( inoutsig, 0 );
+  cutofffreq=2000;
+  [b, a] = butter(1, cutofffreq*2/fs);
+  for ii=1:5
+    inoutsig = filter(b,a, inoutsig);
+  end;
  case 'dau'
-   cutofffreq=1000;
-   [b, a] = butter(2, cutofffreq*2/fs);
-   inoutsig = filter(b,a, inoutsig);
+  inoutsig = max( inoutsig, 0 );
+  cutofffreq=1000;
+  [b, a] = butter(2, cutofffreq*2/fs);
+  inoutsig = filter(b,a, inoutsig);
  case 'hilbert'
   inoutsig = abs(hilbert(inoutsig));
  case 'lindemann'
-   cutofffreq=800;
-   [b, a] = butter(1, cutofffreq*2/fs);
-   inoutsig = filter(b,a, inoutsig);
+  inoutsig = max( inoutsig, 0 );
+  cutofffreq=800;
+  [b, a] = butter(1, cutofffreq*2/fs);
+  inoutsig = filter(b,a, inoutsig);
  otherwise
   error('%s: Unknown method name: %s.',upper(mfilename),methodname);
 end;
