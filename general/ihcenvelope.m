8ad63fc4d3485e6387405ee1f0edc299e1f77ec0
Peter L. Soendergaard <peter@pspc.elektro.dtu.dk>
2010-01-21 13:44:10 +0100

    TFS calculation was missing for the Bernstein option in ihcenvelope

--- general/ihcenvelope.m
+++ general/ihcenvelope.m
@@ -1,4 +1,4 @@
-function inoutsig = ihcenvelope(inoutsig,fs,methodname)
+function inoutsig = ihcenvelope(inoutsig,fs,methodname,nonneg)
 %IHCENVELOPE   Inner hair cell envelope extration
 %   Usage:  outsig=ihcenvelope(insig,fs,methodname);
 %
@@ -13,9 +13,12 @@ function inoutsig = ihcenvelope(inoutsig,fs,methodname)
 %   method has been utilized or studied. The options are
 %
 %-    'bernstein' - Compute the Hilbert envelope, compress the envelope
-%                 by raising it to the power .23, half-wave rectify it, 
+%                 by raising it to the power .2, combine the envelope
+%                 with the original fine-structure, half-wave rectify it, 
 %                 square it and low-pass filter it with a cut-off
-%                 frequency of 425 Hz. This method is defined in Bernstein 1999.
+%                 frequency of 425 Hz. This method is defined in
+%                 Bernstein 1999. Note that this method includes both a
+%                 compression and an expansion stage.
 %
 %-    'breebart' - Use a 5th order filter with a cut-off frequency of 770
 %                 Hz. This method is given in Breebart 2001. Page 94 in thesis.
@@ -28,19 +31,24 @@ function inoutsig = ihcenvelope(inoutsig,fs,methodname)
 %-    'hilbert' - Use the Hilbert envelope instead of the half-wave
 %                 rectification and low pass filtering. This is not a
 %                 releastic model of the inner hair envelope extraction
-%                 process, but the option is included for completeness.
+%                 process, but the option is included for
+%                 completeness. The Hilbert envelope was first suggested
+%                 for signal analysis in Gabor 1946.
 %
 %-    'lindemann' - Use a 1st order Butterworth filter with a cut-off frequency
 %                 of 800 Hz. This method is defined in the paper
 %                 Lindemann 1986a.
 %
-%R  bernstein1999normalized breebaart2001binaural lindemann1986a dau1996qmeI
+%   IHCENVELOPE(insig,fs,methodname,'nonneg') ensures that the output is
+%   non-negative by setting negative values to zero.
+%
+%R  bernstein1999normalized breebaart2001binaural gabor1946 lindemann1986a dau1996qmeI
   
 % FIXME: Which paper did this idea originally appear in?
 
 % ------ Checking of input parameters --------------------------------
 
-error(nargchk(3,3,nargin));
+error(nargchk(3,4,nargin));
 
 if ~isnumeric(inoutsig)
   error('%s: The input signal must be numeric.',upper(mfilename));
@@ -54,16 +62,22 @@ if ~ischar(methodname)
   error('%s: methodname must be a string.',upper(mfilename));
 end;
 
-% ------ Computation -------------------------------------------------
-
-if ~strcmp(lower(methodname),'hilbert')
-  % Half-wave rectification
-  inoutsig = max( inoutsig, 0 );
+if nargin==4
+  if ~ischar(nonneg) 
+    error('%s: last argument must be a string.',upper(mfilename));
+  end;
 end;
+
+end;
+% ------ Computation -------------------------------------------------
 
 switch(lower(methodname))
  case 'bernstein'
-  inoutsig=max(abs(hilbert(inoutsig)).^0.23,0).^2;
+  % The computational trick mentioned in the Bernstein paper is used
+  % here: Instead of raising the envelope to power .23 and combine with its
+  % TFS, we raise it to power -.77, and combine with the original
+  % signal. In this way we avoid computing the fine structure.
+  inoutsig=max(abs(hilbert(inoutsig)).^(-.77).*inoutsig,0).^2;
   cutofffreq=425;
   [b, a] = butter(2, cutofffreq*2/fs);
   inoutsig = filter(b,a, inoutsig);
@@ -89,3 +103,17 @@ switch(lower(methodname))
  otherwise
   error('%s: Unknown method name: %s.',upper(mfilename),methodname);
 end;
+
+if nargin==4
+  switch(lower(nonneg))
+   case 'nonneg'
+    switch(lower(methodname))
+     case {'bernstein','breebart','dau','lindemann'}
+      inoutsig = max( inoutsig, 0 );
+    end;    
+   case ''
+   otherwise
+    error(['Unknown argument.']);
+  end;  
+end;
+
