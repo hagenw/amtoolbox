14fb6ff2bc83d1ef663dee35fb39759ea0b56ffc
Peter L. Soendergaard <peter@pspc.elektro.dtu.dk>
2010-02-08 14:39:11 +0100

    added 'complex' flag to gammatone

---
+++ comp/Contents.m
@@ -0,0 +1,9 @@
+% AMT - Computational functions
+%
+%  The AMT team, 2010.
+%
+%  Input arguments
+%     AMTARGHELPER     - Help parse input arguments.
+%
+%  For help, bug reports, suggestions etc. please send email to
+%  amtoolbox-help@lists.sourceforge.net
