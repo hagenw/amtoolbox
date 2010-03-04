4dc3f66046473ac3e7b06f9ecae881ef0ca4bd8d
Peter L. Soendergaard <peter@pspc.elektro.dtu.dk>
2010-03-04 14:59:09 +0100

    untabify Contents

--- lindemann/Contents.m
+++ lindemann/Contents.m
@@ -1,14 +1,13 @@
 % AMT - Lindemann model function
 %
-%  The AMT team, 2009.
+%  The AMT team, 2010.
 %
 %  General routines
-%     LINDEMANN        	- Calculate a binaural activity map
-%     PLOTLINDEMANN	 		- Plot the binaural activity map
+%     LINDEMANN         - Calculate a binaural activity map
+%     PLOTLINDEMANN     - Plot the binaural activity map
 %
 %  Cross-correlation
-%     BINCORR		        - Calculates a running cross-correlation between two
-%     										signals
+%     BINCORR           - Running cross-correlation between two signals
 %
 %  For help, bug reports, suggestions etc. please send email to
 %  amtoolbox-help@lists.sourceforge.net
