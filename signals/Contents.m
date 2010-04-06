81851df0984746e3be771f04b8ac6771f73383a4
Hagen Wierstorf <hagen.wierstorf@telekom.de>
2010-04-06 13:38:14 +0200

    Added file to generate a sinusoid with a given ITD

--- signals/Contents.m
+++ signals/Contents.m
@@ -1,8 +1,10 @@
 % AMT - Signals
 %
 %  Sound signals.
+%    BMSIN          -  Binaural modulated sinusoid
 %    GREASY         -  Woman speaking the word 'greasy'
 %    GSPI           -  Glockenspiel test signal.
+%    ITDSIN         -  Sinusoid with interaural time difference
 %
 %  Other test signals are available from:
 %
