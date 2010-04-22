1d79fdbb3bfc6a4ae3d2f51cab68e27f1b9acc05
Hagen Wierstorf <hagen.wierstorf@telekom.de>
2010-04-22 12:40:47 +0200

    Added a progressbar function for loops

---
+++ comp/progressbar.m
@@ -0,0 +1,34 @@
+function progressbar(ii,nii)
+% PROGRESSBAR Show the progress of an iteration
+%   Usage: progressbar(ii,nii)
+%
+%   Input parameters:
+%       ii  - Current iteration
+%       nii - number of iterations
+%
+%   PROGRESSBAR(ii,nii) displays the progress of a loop.
+%
+
+%   AUTHOR: Hagen Wierstorf
+
+
+% ------ Checking of input parameters ------------------------------------
+
+error(nargchk(2,2,nargin));
+
+if ( ~isnumeric(ii) || ~isscalar(ii) )
+    error('%s: ii has to be a scalar.',upper(mfilename));
+end
+
+if ( ~isnumeric(nii) || ~isscalar(ii) )
+    error('%s: nii has to be a scalar.',upper(mfilename));
+end
+
+
+% ------ Generate the progress bar ---------------------------------------
+
+if ii==nii
+    fprintf(1,'\rRun %.0f/%.0f\n',ii,nii);
+else
+    fprintf(1,'\rRun %.0f/%.0f',ii,nii);
+end
