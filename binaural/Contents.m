680e48e54650950868ae01d62a9a36306737bf7c
Peter L. Soendergaard <soender@users.sourceforge.net>
2010-07-02 14:35:43 +0200

    removed interauralcoh

--- binaural/Contents.m
+++ binaural/Contents.m
@@ -7,7 +7,6 @@
 %     PLOTLINDEMANN     - Plot the binaural activity map
 %     LINDEMANNWIN      - Apply a N_1/2 onset window to the signal
 %     LINDCENTROID      - Centroid of the cross-correlation activation
-%     INTAURALCOH       - Calculates the interaural coherence
 %     BINCORR           - Running cross-correlation between two signals
 %
 %  For help, bug reports, suggestions etc. please send email to
