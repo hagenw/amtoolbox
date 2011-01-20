6a1ba1726cf7512a98d53c881933d49fe4a351e4
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-01-20 15:00:57 +0100

    reworked Culling's model

---
+++ binaural/culling2010.m
@@ -0,0 +1,88 @@
+function [benefit, weighted_SNR, weighted_bmld] = culling2010(target,interferer,fs,varargin)
+%CULLING2010  Binaural advantage for speech in reverberant conditions
+%   Usage:  [benefit weighted_SNR weighted_bmld] = culling2010(target,interferer,fs)
+%  
+%   Input parameters:
+%     target       - Binaural target stimuli
+%     interfererer - Binaural interferer stimuli
+%    
+%   CULLING2010(target,interferer,fs) will compute the increase in speech
+%   intelligibility of the target when listening binaurally to the target
+%   and interferer. The signals are assumed to be sampled at a sampling
+%   frequency of fs Hz.
+% 
+%R  culling2010mapping culling2007evidence
+  
+definput.flags.ears={'both','left','right'};
+[flags,kv]=ltfatarghelper({},definput,varargin);
+
+% Make sure that there is slightly more than 1 erb per channel, and get
+% the gammatone filters.
+nchannels=ceil(freqtoerb(fs/2));
+fc=erbspace(0,fs/2,nchannels);
+[b,a] = gammatone(fc,fs,'complex');
+
+effective_SNR = zeros(nchannels,1);
+bmld_prediction = zeros(nchannels,1);
+
+targ_f = 2*real(filterbankz(b,a,target));
+int_f  = 2*real(filterbankz(b,a,interferer));
+
+for n = 1:nchannels
+  % Calculate the effect of BMLD
+  if flags.do_both
+    % cross-correlate left and right signal in channel n for both the
+    % target and the inteferer
+    [phase_t, coher_t] = do_xcorr(targ_f(:,n,1),targ_f(:,n,2),fs,fc(n)); 
+    [phase_i, coher_i] = do_xcorr( int_f(:,n,1), int_f(:,n,2),fs,fc(n)); 
+
+    bmld_prediction(n) = culling2007bmld(coher_t,phase_t,phase_i,fc(n));
+  end
+
+  % Calculate the effect of better-ear SNR
+  left_SNR  = sum(targ_f(:,n,1).^2) / sum(int_f(:,n,1).^2);
+  right_SNR = sum(targ_f(:,n,2).^2) / sum(int_f(:,n,2).^2);
+
+  if flags.do_both
+    SNR = max(left_SNR,right_SNR);
+  end;
+
+  if flags.do_left
+    SNR = left_SNR;
+  end;
+  
+  if flags.do_right
+    SNR = right_SNR;
+  end
+  
+  % combination
+  effective_SNR(n) = 10*log10(SNR);
+end
+
+% Calculate the SII weighting
+weightings = siiweightings(fc);
+
+if flags.do_both
+  weighted_bmld = sum(bmld_prediction.*weightings);
+else
+  weighted_bmld = 0;
+end
+
+weighted_SNR = sum(effective_SNR.*weightings);
+
+benefit = weighted_SNR + weighted_bmld;
+
+
+% Helper function to do the cross-correlation, and extract the delay of
+% the peak (output parameter 'phase' and the coherence at the peak.
+function [phase coherence] = do_xcorr(left, right, fs, fc)
+  % Compute maximum lag to be considered. This could be very large for a
+  % centre frequency close to 0, so set a maximum.
+  maxlag = min(length(left),round(fs/(fc*2)));
+  
+  % Compute the correlation
+  iacc = xcorr(squeeze(left),squeeze(right),maxlag,'coeff');
+  
+  % Find the position of the large correlation coefficient.
+  [coherence delay_samp] = max(iacc);
+  phase = fc*2*pi*delay_samp/fs;
---
+++ modelstages/culling2007bmld.m
@@ -0,0 +1,11 @@
+function [bmld_out] = culling2007bmld(coherence,phase_target,phase_int,fc)
+%CULLING2007BLMD  Binaural Masking Level Difference 
+%
+%R  culling2007evidence
+  
+k = (1 + 0.25^2) * exp((2*pi*fc)^2 * 0.000105^2);
+bmld_out = 10 * log10 ((k - cos(phase_target-phase_int))/(k - coherence));
+if bmld_out < 0;
+    bmld_out = 0;
+end
+return
