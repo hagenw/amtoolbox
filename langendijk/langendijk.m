b60686b49af3e3d581474175b4eb501b21cc26e3
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-03-10 15:59:54 +0100

    updating langendijk, 1

--- langendijk/langendijk.m
+++ langendijk/langendijk.m
@@ -1,80 +1,103 @@
-function [ p ] = langendijk( ir1,ir2,bw,do,cp,s,bal,fstart,fend,stim )
-% LANGENDIJK Localization model according to Langendijk et al. (2002)
-% Usage:    [ p ] = langendijk( ir1,ir2 )
-%           [ p ] = langendijk( ir1,ir2,stim )
-%           [ p ] = langendijk( ir1,ir2,bw,do,cp,s,bal,fstart,fend )
-%           [ p ] = langendijk( ir1,ir2,bw,do,cp,s,bal,fstart,fend,stim )
-% Input arguments:
-%     ir1:      modified impulse responses of DTFs for all positions and
-%               both ears (sorted)
-%     ir2:      stored impulse responses of DTF-templates for all positions 
-%               and both ears (sorted)
-%     bw:       bandwidth of filter bands as partial of an octave; default: 6
-%     do:       differential order; default: 0
-%     cp:       comparison process; 'std' (default) or 'xcorr'
-%     s:        standard deviation of transforming Gaussian function; default: 2
-%     bal:      balance of left to right channel (not included in 
-%               langendijk's original comparison process); default: 1
-%     fstart:   start frequency of filter bank; min: 0,5kHz; default: 2kHz
-%     fend:     end frequency of filter bank; default: 16kHz
-%     stim:     applied stimulus for localization test (optional)
-% Output argument:
-%     p:        predicted probability density functions for response angles 
-%               with respect to target positions
-% Description:
-% p = langendijk( ir1,ir2,... ) results to a two dimensional matrix p. 
-% The first dimension represents all possible response positions in 
-% increasing order and the second dimension all possible target 
-% respectively source positions. Consequently each column describes the 
-% probability density function of the response distribution for one special 
-% target position.
-% If you want to plot this matrix use plotlangendijk().
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function p = langendijk(ir1,ir2,varargin)
+%LANGENDIJK Localization model according to Langendijk et al. (2002)
+%   Usage:    p = langendijk(ir1,ir2)
+%             p = langendijk(ir1,ir2,bw,do,cp,s,bal,flow,fhigh,stim)
+%
+%   Input argumentss:
+%     ir1     : modified impulse responses of DTFs for all positions and both ears (sorted)
+%     ir2     : stored impulse responses of DTF-templates for all positions and both ears (sorted)
+%
+%   Output arguments:
+%     p       : Predicted probability density functions for response angles with respect to target positions
+%
+%   LANGENDIJK(ir1,ir2,... ) results to a two dimensional matrix p.  The
+%   first dimension represents all possible response positions in
+%   increasing order and the second dimension all possible target
+%   respectively source positions. Consequently each column describes the
+%   probability density function of the response distribution for one
+%   special target position. If you want to plot this matrix use
+%   PLOTLANGENDIJK.
+%
+%   LANGENDIJK accepts the following optional parameters.
+%  
+%-     'bw',bw : Bandwidth of filter bands as partial of an octave. The
+%                default value is 6.
+%
+%-     'do',do : Differential order. The default value is 0.
+%
+%-     'cp':cp : Comparison process; 'std' (default) or 'xcorr'.
+%
+%-     's',s   : Standard deviation of transforming Gaussian function; default: 2
+%
+%-     'bal',bal : Balance of left to right channel (not included in 
+%                langendijk's original comparison process); default: 1
+%
+%-     'flow',flow : Start frequency of filter bank. min: 0,5kHz; default: 2kHz
+%
+%-     'fhigh',fhigh : End frequency of filter bank; default: 16kHz
+%
+%-     stim:     applied stimulus for localization test (optional)
+%
+%   See also: plotlangendijk
+
 % AUTHOR : Robert Baumgartner, OEAW Acoustical Research Institute
-% latest update: 2010-07-19
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+definput.keyvals.bw=6;
+definput.keyvals.do=0;
+definput.keyvals.cp='std';
+definput.keyvals.s=2;
+definput.keyvals.bal=1;
+definput.keyvals.flow=2000;
+definput.keyvals.fhigh=16000;
+definput.keyvals.stim=[];
 
-% default settings according to Langendijk et al. (2002)
-if ~exist('bw','var')
-    bw=6;
-end
-if ~exist('do','var')
-    do=0;
-end
-if ~exist('cp','var')
-    cp='std';
-end
-if ~exist('s','var')
-    s=2;
-end
-if ~exist('bal','var')
-    bal=1;
-end
-if ~exist('fstart','var')
-    fstart=2000;
-end
-if ~exist('fend','var')
-    fend=16000;
-end
+[flags,kv]=ltfatarghelper({'bw','do','cp','s','bal','flow','fhigh','stim'},definput,varargin);
 
-% addition for applied stimulus
-if length(bw)>1
-    stim=bw;
-end
-if exist('stim','var')
-    ir1=halfconv( ir1,stim );
+if ~isempty(kv.stim)
+  ir1=halfconv( ir1,stim );
 end
 
 % model calculations
 p=zeros(size(ir2,2),size(ir1,2)); % initialisation
+
 % filter bank
-for ind2=1:size(ir1,2) % response pdf for every target position
-    x=averagingfb(ir1(:,ind2,:),bw,fstart,fend);
-    y=zeros(length(x),size(ir2,2),size(ir2,3)); % initialisation
-    for ind=1:size(y,2) % response pdf for one target position
-        y(:,ind,:)=averagingfb(ir2(:,ind,:),bw,fstart,fend);
-    end
-% comparison process for one target position
-    p(:,ind2)=langecomp( x,y,s,do,cp,bal );
+% response pdf for every target position
+for ind2=1:size(ir1,2) 
+  x=averagingfb(ir1(:,ind2,:),kv.bw,kv.flow,kv.fhigh);
+  y=zeros(length(x),size(ir2,2),size(ir2,3)); % initialisation
+  for ind=1:size(y,2) % response pdf for one target position
+    y(:,ind,:)=averagingfb(ir2(:,ind,:),kv.bw,kv.flow,kv.fhigh);
+  end
+
+  % comparison process for one target position
+  p(:,ind2)=langecomp( kv.x,kv.y,kv.s,kv.do,kv.cp,kv.bal );
 end
+
+
+
+function [ outsig ] = halfconv( ir1,stim )
+% HALFCONV calculates the fast convolution with fft but without ifft
+% Usage:        [ outsig ] = halfconv( ir1,stim )
+% Input arguments:
+%     ir1:      (modified) impulse responses of DFTs for all positions and
+%               both ears
+%     stim:     stimulus (time domain)
+% Output argument:
+%     outsig:   convolution of stimulus with DTFs in frequency domain
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% AUTHOR : Robert Baumgartner, OEAW
+% latest update: 2010-07-19
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+nfft = 2^nextpow2(max([size(ir1,1) length(stim)]));
+stimf=fft(stim(:),nfft);
+ir1f = fft(ir1,nfft,1);
+temp=zeros(nfft,size(ir1,2),size(ir1,3));
+for ch=1:size(ir1,3)
+    for ind=1:size(ir1,2)
+        temp(:,ind,ch) = ir1f(:,ind,ch).* stimf;
+    end
+end
+outsig=temp;
+
 end
