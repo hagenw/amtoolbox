bc11a43360c5162d5811a68af1efc58e33f452b7
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-03-11 15:07:28 +0100

    more demo

--- demos/demo_absolutethreshold.m
+++ demos/demo_absolutethreshold.m
@@ -1,7 +1,7 @@
-%DEMO_ABSOLUTETHRESHOLD  Plot of the different absolute thresholds
+%DEMO_ABSOLUTETHRESHOLD  Standards for absolute threshold of hearing
 %
 %  This demos generates a simple figure that shows the behaviour of
-%  the different absolute thresholds of hearing.
+%  the different standards for absolute thresholds of hearing.
 %
 %  FIGURE 1  Thresholds of hearing by standard
 %
@@ -24,6 +24,7 @@ xrange=linspace(flow,fhigh,plotpoints);
 figure(1)
 
 types   = {'iso226_2003','map','er3a','er2a'};
+legends = {'iso226\_2003','map','er3a','er2a'};
 symbols = {'k-'         ,'ro' ,'gx'  ,'b+'  };
 
 hold on;
@@ -31,7 +32,7 @@ for ii=1:numel(types)
   semiaudplot(xrange,absolutethreshold(xrange,types{ii}),'opts',{symbols{ii}});
 end;
 hold off;
-legend(types{:},'Location','SouthEast');
+legend(legends{:},'Location','SouthEast');
 xlabel('Frequency (Hz)');
 ylabel('Absolte threshold (dB SPL)');
 
