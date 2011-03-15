0e5e2e58bb940be469aea92b4003ea1a94e79c61
eggerk <egger.kathi@gmail.com>
2011-03-15 14:10:36 +0100

    * updating langendijk, 3
    Signed-off-by: eggerk <egger.kathi@gmail.com>

--- binaural/langendijkcomp.m
+++ binaural/langendijkcomp.m
@@ -26,7 +26,7 @@ function [ out ] = langendijkcomp( in1,in2,varargin )
         if kv.do==0
           z=in1(:,ch)-in2(:,ind,ch);
         else
-          z=diff(in1(:,ch),do)-diff(in2(:,ind,ch),do);
+          z=diff(in1(:,ch),kv.do)-diff(in2(:,ind,ch),kv.do);
         end
         sigma=std(z,1);
         ptemp(ind,ch)=normpdf(sigma,0,kv.s);
@@ -35,7 +35,7 @@ function [ out ] = langendijkcomp( in1,in2,varargin )
     if size(in2,3)==1
       p=ptemp;
     else
-      p=(bal*ptemp(:,1)+1/bal*ptemp(:,2))/2; % balance
+      p=(kv.bal*ptemp(:,1)+1/kv.bal*ptemp(:,2))/2; % balance
     end
     p=1/sum(p).*p;
     % p=p/max(p); % normalisation
@@ -50,7 +50,7 @@ function [ out ] = langendijkcomp( in1,in2,varargin )
         if kv.do==0
           z=corrcoef(in1(:,ch),in2(:,ind,ch));
         else
-          z=corrcoef(dif(in1(:,ch),do),dif(in2(:,ind,ch),do));
+          z=corrcoef(dif(in1(:,ch),kv.do),dif(in2(:,ind,ch),kv.do));
         end
         z=z(2);
         ptemp(ind,ch)=(z+1)/2;
@@ -59,7 +59,7 @@ function [ out ] = langendijkcomp( in1,in2,varargin )
     if size(in2,3)==1
       p=ptemp;
     else
-      p=(bal*ptemp(:,1)+1/bal*ptemp(:,2))/2; % balance
+      p=(kvbal*ptemp(:,1)+1/kv.bal*ptemp(:,2))/2; % balance
     end
   
   end
--- humandata/Contents.m
+++ humandata/Contents.m
@@ -4,6 +4,8 @@
 %
 %   Data routines
 %       DATA_LINDEMANN1986A  - Data from the lindemann1986a paper.
+%       data_langendijk2002 - Returns response patterns and DTF data of
+%                             given figures in Langendijk & Bronkhorst 2002
 %
 %  For help, bug reports, suggestions etc. please send email to
 %  amtoolbox-help@lists.sourceforge.net
