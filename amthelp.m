46989dcc5fff60e71586f8990b6aed5a91a6eabb
Peter L. Soendergaard <soender@users.sourceforge.net>
2012-11-08 10:24:13 +0100

    modernized amthelp

--- amthelp.m
+++ amthelp.m
@@ -1,17 +1,15 @@
-function op1=amthelp(q,modulename)
-%AMTHELP Help on the AMT model
-%   Usage:  amthelp;
-%           v=amthelp('version');
-%           mlist=amthelp('modules');
+function op1=amthelp(varargin)
+%AMTHELP Help on the AMToolbox
+%   Usage:  ltfathelp;
+%           v=ltfathelp('version');
+%           mlist=ltfathelp('modules');
 %
-%   `amthelp` displays some general help on the AMT toolbox..
+%   `amthelp` displays some general help on the AMToolbox.
 %
 %   `amthelp('version')` returns the version number.
 %
 %   `amthelp('modules')` returns a cell array of installed modules and
 %   corresponding version numbers.
-%
-%   `amthelp('authors')` lists the name of the authors.
 %
 %   See also:  amtstart
 
@@ -19,24 +17,31 @@ function op1=amthelp(q,modulename)
 %   TESTING: NA
 %   REFERENCE: NA
 
-% Verify that ltfatarghelper is in path
-% Verify that ltfatarghelper is in path
-if ~exist('ltfatarghelper','file')
-  disp(' ');
-  disp('--- AMT - Auditory Modelling Toolbox. ---');
-  disp(' ')
-  disp('To use the AMToolbox, you must first install LTFAT and call LTFATSTART as the first command.');
-  disp(' ');
-  return;
+% Verify that LTFAT has been installed
+if ~exist('ltfatarghelper','file')  
+    disp('');
+    disp('--- AMTOOLBOX - The Auditory Modelling toolbox. ---');
+    disp('')
+    error(['The toolbox require the LTFAT toolbox to properly function. ' ...
+         'Please download and install it from http://ltfat.sourceforge.net,' ...
+         'and then call the LTFATSTART command BEFORE you call ' ...
+          'AMTSTART.'])
 end;
 
+bp=amtbasepath;
 
-if nargin==0
+definput.keyvals.versiondata=[];
+definput.keyvals.modulesdata=[];
+definput.flags.mode={'general','version','modules','authors'};
+
+[flags,kv]=ltfatarghelper({},definput,varargin);
+
+if flags.do_general
   disp(' ');
-  disp('--- AMT - Auditory Modelling Toolbox. ---');
+  disp('--- AMT - The Auditory Modelling Toolbox. ---');
   disp(' ')
 
-  disp(['Version ',AMT_CONF.amt_version]);
+  disp(['Version ',kv.versiondata]);
   disp(' ');
   disp('Installed modules:');
   disp(' ');
@@ -59,81 +64,32 @@ if nargin==0
 
   end; 
   disp(' ');
-  disp('For other questions, please don''t hesitate to send an email to amt-help@lists.sourceforge.net.'); 
+  disp('For other questions, please don''t hesitate to send an email to amtoolbox-help@lists.sourceforge.net.'); 
     
-  return
 end;
   
-if ischar(q);
+if flags.do_version
+  op1=kv.versiondata;
+end;
 
-  bp=AMT_CONF.basepath;
-
-  switch(lower(q))
-    case {'v','version'}
-      if nargin==1
-	op1=AMT_CONF.amt_version;
-      else
-	for ii=1:length(AMT_CONF.modules);
-      % strcmpi does not exist in older versions of Octave,
-      % therefore use strcmp(lower(...
-	  if strcmp(lower(modulename),AMT_CONF.modules{ii}.name)
-	    found=1;
-	    op1=AMT_CONF.modules{ii}.version;
-	    return
-	  end;
-	end;
-
-	% If we get here, it means that no module matched 'modulename'.
-	error('Unknown module name.');
-				   
-      end;
-    case {'m','modules'}
-      op1={};
-      for ii=1:length(AMT_CONF.modules);
-
-	p=AMT_CONF.modules{ii};
-
-	% Get the first line of the help file
-	[FID, MSG] = fopen ([bp,p.name,filesep,'Contents.m'],'r');
-        if FID==-1
-          error('Module %s does not contain a Contents.m file.',p.name);
-	end;
-	firstline = fgetl (FID);
-	fclose(FID);
-
+if flags.do_modules
+  op1={};
+  for ii=1:numel(kv.modulesdata)
     
-	% Load the information into the cell array.	
-	op1{ii}.name=p.name;
-	op1{ii}.version=p.version;
-	op1{ii}.description=firstline(2:end);
-        
-      end;
-    case {'a','authors'}
-      disp('Peter L. Soendergaard, Centre for Applied Hearing Research,');
-      disp('                       Technical University of Denmark.');
-      disp(' ');
-      disp('Tobias May,            Phillips Research Laboratories,');
-      disp('                       Eindhoven.');
-      
-    otherwise
-      found=0;
-      for ii=1:length(AMT_CONF.modules);
-	if strcmp(lower(q),AMT_CONF.modules{ii}.name)
-	  found=1;
-	  p=AMT_CONF.modules{ii};
-
-	  if isoctave
-	    s=pwd;
-	    cd([bp,p.name]);
-	    help Contents
-	    cd(s);
-	  else
-	    help(lower(q))
-	  end;
-	end;
-      end;  
-      if ~found
-	error('Unknown command or module name.');
-      end;
+    p=kv.modulesdata{ii};
+    
+    % Get the first line of the help file
+    [FID, MSG] = fopen ([bp,p.name,filesep,'Contents.m'],'r');
+    if FID==-1
+      error('Module %s does not contain a Contents.m file.',p.name);
+    end;
+    firstline = fgetl (FID);
+    fclose(FID);
+    
+    
+    % Load the information into the cell array.	
+    op1{ii}.name=p.name;
+    op1{ii}.version=p.version;
+    op1{ii}.description=firstline(2:end);
   end;
 end;
