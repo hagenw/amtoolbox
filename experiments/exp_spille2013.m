4eff3b33258617170f4cb5ef004720834800e8fe
Peter L. Soendergaard <soender@users.sourceforge.net>
2013-04-10 10:51:10 +0200

    recreated exp_spille2013

--- experiments/exp_spille2013.m
+++ experiments/exp_spille2013.m
@@ -4,6 +4,16 @@ function example_output = exp_spille2013
 %   `exp_spille2013` reproduce Fig. no. 6 from the Spille et
 %   al. 2013 AABBA chapter.
 %
+%   Examples:
+%   ---------
+%
+%   To display Fig. 6 use :::
+%
+%     exp_spille2013;
+%
+%   See also: exp_dietz2011
+
+
 % AUTHOR: Mathias Dietz
 
 % choose input signal (e.g. spille2013_s123456)
