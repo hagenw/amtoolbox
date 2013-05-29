4999ba7811cbe75f8c65f320abf4e602d9d2be1e
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-05-29 16:58:04 +0200

    Added fig 1 and 3

--- experiments/exp_wierstorf2013.m
+++ experiments/exp_wierstorf2013.m
@@ -73,25 +73,25 @@ if flags.do_fig1
       
     % listening area
     X = [-2 2];
-    Y = [-3.15 0.15];
+    Y = [-3.15 -0.15];
     % orientation of the listener (always to the front)
-    phi = -pi/2;
+    phi = pi/2;
     % position of the virtual point source
     xs = [0 0];
     src = 'ps';
     % distance between the loudspeaker ion the stereo setup
     L = 2;
-    % points in the listening area: resolution x resolution
-    resolution = 21;
   
     if amtredofile(s,flags.redomode)
         [loc_error,aud_event,sound_event,xaxis,yaxis,x0] = ...
-            wierstorf2013(X,Y,phi,xs,src,L,resolution,'stereo');
+            wierstorf2013(X,Y,phi,xs,src,L,'stereo');
         save(save_format,s,'loc_error','aud_event','xaxis','yaxis','x0');
     else
         s = load(s);
         loc_error = s.loc_error;
         aud_event = s.aud_event;
+        xaxis = s.xaxis;
+        yaxis = s.yaxis;
         x0 = s.x0;
     end;
 
@@ -104,11 +104,92 @@ if flags.do_fig1
     if flags.do_plot
         % ------ Plotting ------
         figure;
-        imagesc(xaxis,yaxis,abs(degree(loc_error)));
-        draw_loudspeakers(x0,[1 1]);
+        [u,v,~] = pol2cart(rad(aud_event+90),ones(size(aud_event)), ...
+            zeros(size(aud_event)));
+        quiver(xaxis,yaxis,u',v',0.5);
+        axis([-2.13 2.13 -3.3 0.2])
+        draw_loudspeakers(x0,1);
         xlabel('x/m');
         ylabel('y/m');
-        title('sweet spot in stereophony');
     end;
 
+elseif flags.do_fig3
+
+    s = [mfilename('fullpath'),'_fig3.mat'];
+
+    conf = SFS_config;  
+    % listening area
+    X = [-2 2];
+    Y = [-2 2];
+    % circular array with 56 loudspeakers
+    L = 3;
+    conf.array = 'circle';
+    conf.dx0 = L*pi/56;
+    conf.xref = [0 0];
+    % plane wave travelling upwards
+    xs = [0 1];
+    src = 'pw';
+  
+    if amtredofile(s,flags.redomode)
+        % get secondary sources and tapering window for plotting
+        x0 = secondary_source_positions(L,conf);
+        win = [zeros(1,29) ones(1,27)];
+        % (a)
+        f = 1000;
+        [xaxis,yaxis,P_a] = wave_field_mono_wfs_25d(X,Y,xs,src,f,L,conf);
+        % (b)
+        f = 2000;
+        [~,~,P_b] = wave_field_mono_wfs_25d(X,Y,xs,src,f,L,conf);
+        % (c)
+        f = 5000;
+        [~,~,P_c] = wave_field_mono_wfs_25d(X,Y,xs,src,f,L,conf);
+        % (d)
+        t = 212;
+        [~,~,p_d] = wave_field_imp_wfs_25d(X,Y,xs,src,t,L,conf);
+        save(save_format,s,'P_a','P_b','P_c','p_d','xaxis','yaxis','x0','win');
+    else
+        load(s);
+    end;
+
+    output.P_a = P_a;
+    output.P_b = P_b;
+    output.P_c = P_c;
+    output.p_d = p_d;
+    output.xaxis = xaxis;
+    output.yaxis = yaxis;
+    output.x0 = x0;
+    output.win = win;
+
+    if flags.do_plot
+        % ------ Plotting ------
+        % (a)
+        plot_wavefield(xaxis,yaxis,P_a,x0,win,conf);
+        axis([X(1) X(2) Y(1) Y(2)]);
+        colorbar;
+        xlabel('x/m');
+        ylabel('y/m');
+        title('(a) f_{pw} = 1kHz');
+        % (b)
+        plot_wavefield(xaxis,yaxis,P_b,x0,win,conf);
+        axis([X(1) X(2) Y(1) Y(2)]);
+        colorbar;
+        xlabel('x/m');
+        ylabel('y/m');
+        title('(b) f_{pw} = 2kHz');
+        % (c)
+        plot_wavefield(xaxis,yaxis,P_c,x0,win,conf);
+        axis([X(1) X(2) Y(1) Y(2)]);
+        colorbar;
+        xlabel('x/m');
+        ylabel('y/m');
+        title('(c) f_{pw} = 5kHz');
+        % (d)
+        conf.plot.usedb = 1;
+        plot_wavefield(xaxis,yaxis,p_d,x0,win,conf);
+        axis([X(1) X(2) Y(1) Y(2)]);
+        colorbar;
+        xlabel('x/m');
+        ylabel('y/m');
+        title('(d) t_{pw} = 4.8ms');
+    end
 end;
