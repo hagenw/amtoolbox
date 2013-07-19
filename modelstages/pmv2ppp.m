df9728a2573fb270727d0ac2690c4355b1116374
baumsource <baumsource@gmail.com>
2013-07-19 14:38:14 +0200

    elevation bias updated

--- modelstages/pmv2ppp.m
+++ modelstages/pmv2ppp.m
@@ -1,23 +1,23 @@
 function [ varargout ] = pmv2ppp( p,varargin )
 %PMV2PPP PMV to PPP conversion
-%   Usage:  [ qe,pe,pb ] = pmv2ppp( p,tang,rang );
-%           [ qe,pe,pb ] = pmv2ppp( p,tang,rang,exptang );
+%   Usage:  [ qe,pe,eb ] = pmv2ppp( p,tang,rang );
+%           [ qe,pe,eb ] = pmv2ppp( p,tang,rang,exptang );
 %
 %   Input parameters:
 %     p          : prediction matrix (response PMVs)
 %     tang       : possible polar target angles. As default, ARI's MSP 
 %                  polar angles in the median SP is used.
 %     rang       : polar angles of possible response angles.
-%                  As default regular 5°-sampling is used (-30:5:210).    
+%                  As default regular 5 deg.-sampling is used (-30:5:210).    
 %
 %   Output parameters:
 %     qe         : quadrant error rate
 %     pe         : local polar RMS error in degrees
-%     pb         : polar bias in degrees
+%     eb         : elevation bias in degrees; QEs and up-rear quadrant excluded
 %
 %   `pmv2ppp(...)` retrieves commonly used PPPs (Psychoacoustic performance
 %   parameters) for sagittal-plane (SP) localization like quadrant error
-%   (QEs), local polar RMS error (PE), and local polar bias (PB) from
+%   (QEs), local polar RMS error (PE), and elevation bias (EB) from
 %   response PMVs (probability mass vectors) predicted by a localization
 %   model. PPPs are retreived either for a specific polar target angle or as
 %   an average across all available target angles. The latter is the
@@ -57,54 +57,66 @@ nt = length(tang);
 
 qet = zeros(nt,1); % QE for each target angle
 pet = zeros(nt,1); % PE for each target angle
-pbt = zeros(nt,1); % PB for each target angle
+ebt = zeros(nt,1); % EB for each target angle
+isnotuprear = false(nt,1);
 for ii = 1:nt % for all target positions
     d = tang(ii)-rang;                 % wraped angular distance between tang & rang
     iduw = (d < -180) | (180 < d);     % 180°-unwrap indices
-    d(iduw) = mod(d(iduw) + 180,360) - 180; % 180° unwrap
+    d(iduw) = mod(d(iduw) + 180,360) - 180; % 180 deg unwrap
     d = abs(d);                        % absolut distance
     qet(ii) = sum( p(d>=90,ii) );
     pc = p(d<90,ii);                   % pmv for conditional probability excluding QEs
     pc = pc/sum(pc);                   % normalization to sum=1
     pet(ii) = sqrt( sum( pc .* (d(d<90)).^2 )); % RMS of expected difference
-    pbt(ii) = sum( pc .* rang(d<90) ) - tang(ii); % expectancy value of rang - tang
+    if tang(ii) < 80
+      ebt(ii) = sum( pc .* rang(d<90) ) - tang(ii); % expectancy value of rang - tang
+      isnotuprear(ii) = true;
+    elseif tang(ii) > 180 % elevation instead of polar angle
+      ebt(ii) = -( sum( pc .* rang(d<90) ) - tang(ii) );
+    else % exclude up-rear quadrant
+      isnotuprear(ii) = false;
+    end
 end
+ebt = ebt(isnotuprear);
 
 if ~isempty(kv.exptang)
   
     qetb = (qet(1)+qet(end))/2;  % boundaries for extang
     petb = (pet(1)+pet(end))/2;
-    pbtb = (pbt(1)+pbt(end))/2;
+    ebtb = (ebt(1)+ebt(end))/2;
     
     extang = tang(:); % extended tang for targets outside
     exqet = qet(:);
     expet = pet(:);
-    expb = pbt(:);
+    expb = ebt(:);
     if min(extang)>-90; 
       extang = [-90; extang]; 
       exqet = [qetb; exqet];
       expet = [petb; expet];
-      expb = [pbtb; expb];
+      expb = [ebtb; expb];
+      isnotuprear = [true;isnotuprear];
     end
     if max(extang)<270; 
       extang = [extang; 270]; 
       exqet = [exqet; qetb];
       expet = [expet; petb];
-      expb = [expb; pbtb];
+      expb = [expb; ebtb];
+      isnotuprear = [isnotuprear;true];
     end
     
     qet = interp1(extang,exqet,kv.exptang);
     pet = interp1(extang,expet,kv.exptang);
-    pbt = interp1(extang,expb,kv.exptang);
+    excluderu = kv.exptang < 80 | kv.exptang > 180;
+    ebt = interp1(extang(isnotuprear),expb,kv.exptang(excluderu));
 end
 
 qe = mean(qet)*100;
 pe = mean(pet);
-pb = mean(pbt);
+eb = mean(ebt);
 
 varargout{1} = qe;
 varargout{2} = pe;
-varargout{3} = pb;
+varargout{3} = eb;
 
 
 if flags.do_print
@@ -113,7 +125,7 @@ if flags.do_print
       fprintf('Local polar RMS error (deg) \t %4.1f \n',pe)
     end
     if nargout > 2
-      fprintf('Local polar bias (deg) \t\t %4.1f \n',pb)
+      fprintf('Local polar bias (deg) \t\t %4.1f \n',eb)
     end
 end
 
