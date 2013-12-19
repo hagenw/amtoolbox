4004c9d419a03f70c7f79d109f34a11a63b615d8
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-12-19 19:10:47 +0100

    Updated the estimate_azimuth() function

--- binaural/estimate_azimuth.m
+++ binaural/estimate_azimuth.m
@@ -1,4 +1,4 @@
-function [phi,phi_std,itd,ild,cfreqs] = estimate_azimuth(sig,lookup,model,do_spectral_weighting,fs)
+function [phi,phi_std,itd,ild,cfreqs] = estimate_azimuth(insig,lookup,varargin)
 %ESTIMATE_AZIMUTH Estimate the perceived azimuth using a binaural model
 %   Usage: [phi,itd,ild,cfreqs] = estimate_azimuth(sig,lookup,model,do_spectral_weighting,fs)
 %          [phi,itd,ild,cfreqs] = estimate_azimuth(sig,lookup,model,do_spectral_weighting)
@@ -39,42 +39,38 @@ function [phi,phi_std,itd,ild,cfreqs] = estimate_azimuth(sig,lookup,model,do_spe
 
 
 %% ===== Checking of input  parameters ==================================
-nargmin = 2;
-nargmax = 5;
-error(nargchk(nargmin,nargmax,nargin));
+if nargin<2
+  error('%s: Too few input parameters.',upper(mfilename));
+end;
 
-if nargin==2
-    model = 'dietz2011';
-    do_spectral_weighting = false;
-    fs = 44100;
-elseif nargin==3
-    do_spectral_weighting = false;
-    fs = 44100;
-elseif nargin==4
-    fs = 44100;
+if ~isnumeric(insig) || min(size(insig))~=2
+    error('%s: insig has to be a numeric two channel signal!',upper(mfilename));
 end
-if ~ischar(model)
-    error('%s: %s need to be a string.',upper(mfilename),model);
+
+if ~isstruct(lookup)
+    error('%s: lookup has to be a struct!',upper(mfilename));
 end
-if ~isnumeric(fs) || ~isscalar(fs) || fs<0
-    error('%s: %s need to be a positive scalar.',upper(mfilename),fs);
-end
+
+definput.keyvals.fs = 44100;
+definput.flags.binaural_model = {'dietz','lindemann'};
+definput.flags.spectral_weighting = {'no_spectral_weighting','rms_weighting','raatgever_weighting'};
+
+[flags,kv]  = ltfatarghelper({},definput,varargin);
 
 
 %% ===== Computation ====================================================
 %
-% === Dietz model ===
-if strcmpi('dietz2011',model)
-
+% === Calculate azimuth values for every frequency channel ===
+if flags.do_dietz
     ic_threshold=0.98;
-
     % Run the Dietz model on signal
-    [fine,cfreqs,ild] = dietz2011(sig,fs,'nolowpass','fhigh',1400);
-
+    [fine,cfreqs,ild] = dietz2011(insig,kv.fs,'nolowpass','fhigh',1400,'signal_level_dB_SPL',90);
+    %[fine,cfreqs,ild,env] = dietz2011(insig,kv.fs,'nolowpass','fhigh',5000);
+    %[~,cfreqs,ild,env] = dietz2011(insig,kv.fs,'nolowpass','flow',1400,'fhigh',5000);
     % Unwrap ITDs and get the azimuth values
     itd = dietz2011unwrapitd(fine.itd,ild,fine.f_inst,2.5);
+    %itd = [dietz2011unwrapitd(fine.itd,ild(:,1:12),fine.f_inst,2.5) dietz2011unwrapitd(env.itd,ild(:,13:23),env.f_inst,2.5)];
     phi = itd2angle(itd,lookup);
-
     % Calculate the median over time for every frequency channel of the azimuth
     for n = 1:size(phi,2)
         idx = fine.ic(:,n)>ic_threshold&[diff(fine.ic(:,n))>0; 0]; % compare eq. 9 in Dietz (2011)
@@ -89,16 +85,16 @@ if strcmpi('dietz2011',model)
     % Calculate ITD and ILD values
     %ild = median(ild_tmp,1);
     itd = median(itd,1);
-
-elseif strcmpi('lindemann1986',model)
-
+    % weights for rms-weighting
+    rms_weights = fine.rms;
+elseif flags.do_lindemann
     % run Lindemann model on signal
     c_s = 0.3; % stationary inhibition
     w_f = 0; % monaural sensitivity
     M_f = 6; % decrease of monaural sensitivity
     T_int = inf; % integration time
     N_1 = 1764; % sample at which first cross-correlation is calculated
-    [cc_tmp,~,ild,cfreqs] = lindemann1986(sig,fs,c_s,w_f,M_f,T_int,N_1);
+    [cc_tmp,~,ild,cfreqs] = lindemann1986(insig,kv.fs,c_s,w_f,M_f,T_int,N_1);
     cc_tmp = squeeze(cc_tmp);
     % Calculate tau (delay line time) axes
     tau = linspace(-1,1,size(cc_tmp,1));
@@ -109,47 +105,55 @@ elseif strcmpi('lindemann1986',model)
         itd(jj) = tau(idx)/1000;
     end
     azimuth = itd2angle(itd,lookup);
-else
-    error('%s: %s is not a valid model to choose.',upper(mfilename),model);
+    % TODO: rms weights
 end
 
-% Remove outliers
-[azimuth,cfreqs] = remove_outlier(azimuth,itd,cfreqs);
-% Calculate mean about frequency channels
-if length(azimuth)==0
-    phi = NaN;
-    phi_std = NaN;
-elseif do_spectral_weighting
-    w = spectral_weighting(cfreqs);
-    phi = sum(azimuth.*w)/sum(w);
-    phi_std = [];
-else
-    phi = median(azimuth);
-    phi_std = std(azimuth);
+% === Weights for cross-frequency integration ===
+if flags.do_no_spectral_weighting
+    w = ones(1,12);
 end
-
-end % of main function
-
-%% ===== Subfunctions ====================================================
-function [azimuth,cfreqs] = remove_outlier(azimuth,itd,cfreqs)
-    cfreqs = cfreqs(1:12);
-    % remove unvalid ITDs
-    azimuth = azimuth(abs(itd(1:12))<0.001);
-    cfreqs = cfreqs(abs(itd(1:12))<0.001);
-    % remove NaN
-    azimuth = azimuth(~isnan(azimuth));
-    cfreqs = cfreqs(~isnan(azimuth));
-    % remove outliers more than 30deg away from median
-    if length(azimuth)>0
-        cfreqs = cfreqs(abs(azimuth-median(azimuth))<30);
-        azimuth = azimuth(abs(azimuth-median(azimuth))<30);
-    end
+if flags.do_rms_weighting
+    w = rms_weights;
 end
-function w = spectral_weighting(f)
+if flags.do_raatgever_weighting
     % Calculate a spectral weighting after Stern1988, after the data of
     % Raatgever1980
     b1 = -9.383e-2;
     b2 =  1.126e-4;
     b3 = -3.992e-8;
     w = 10.^( -(b1*f+b2*(f).^2+b3*(f).^3)/10 );
+end
+
+% Remove outliers
+[azimuth,cfreqs,w] = remove_outlier(azimuth,itd,cfreqs,w);
+%[azimuth_env,~,~] = remove_outlier(azimuth_itd(:,13:23),itd,cfreqs,w);
+% Calculate mean about frequency channels
+if length(azimuth)==0
+    phi = NaN;
+    phi_std = NaN;
+else
+    phi = median(azimuth);
+    %phi = sum(azimuth.*w)/sum(w);
+    phi_std = std(azimuth);
+end
+
+end % of main function
+
+%% ===== Subfunctions ====================================================
+function [azimuth,cfreqs,w] = remove_outlier(azimuth,itd,cfreqs,w)
+    %cfreqs = cfreqs(1:12);
+    % remove unvalid ITDs
+    %azimuth = azimuth(abs(itd(1:12))<0.001);
+    %cfreqs = cfreqs(abs(itd(1:12))<0.001);
+    %w = w(abs(itd(1:12))<0.001);
+    % remove NaN
+    azimuth = azimuth(~isnan(azimuth));
+    cfreqs = cfreqs(~isnan(azimuth));
+    w = w(~isnan(azimuth));
+    % remove outliers more than 30deg away from median
+    if length(azimuth)>0
+        cfreqs = cfreqs(abs(azimuth-median(azimuth))<30);
+        w = w(abs(azimuth-median(azimuth))<30);
+        azimuth = azimuth(abs(azimuth-median(azimuth))<30);
+    end
 end
--- general/itd2anglelookuptable.m
+++ general/itd2anglelookuptable.m
@@ -50,6 +50,9 @@ fs = kv.fs;
 nsamples = fs;
 % noise type to use
 noise_type = 'white';
+% SFS Toolbox settings
+conf.ir.useinterpolation = true;
+conf.fs = fs;
 
 
 %% ===== Calculation ====================================================
@@ -71,16 +74,17 @@ if flags.do_dietz2011
     ild = zeros(nangles,23);
     for ii = 1:nangles
         % generate noise coming from the given direction
-        ir = get_ir(irs,[irs.apparent_azimuth(ii) 0 irs.distance]);
-        sig = auralize_ir(ir,sig_noise);
+        ir = get_ir(irs,[irs.apparent_azimuth(ii) 0 irs.distance],conf);
+        sig = auralize_ir(ir,sig_noise,1,conf);
         % calculate binaural parameters
-        [fine, modulation, cfreqs, ild_tmp] = dietz2011(sig,fs);
+        [fine, cfreqs, ild_tmp, env] = dietz2011(sig,fs);
         % unwrap ITD
-        itd_tmp = dietz2011unwrapitd(fine.itd(:,1:12),ild_tmp(:,1:12),fine.f_inst,2.5);
+        itd_tmp = dietz2011unwrapitd(fine.itd,ild_tmp(:,1:12),fine.f_inst,2.5);
+        env_itd_tmp = dietz2011unwrapitd(env.itd,ild_tmp(:,13:23),env.f_inst,2.5);
         % calculate the mean about time of the binaural parameters and store
         % them
-        itd(ii,:) = median(itd_tmp,1);
-        mod_itd(ii,:) = median(modulation.itd,1);
+        itd(ii,1:12) = median(itd_tmp,1);
+        itd(ii,13:23) = median(env_itd_tmp,1);
         ild(ii,:) = median(ild_tmp,1);
     end
 
@@ -90,8 +94,8 @@ elseif flags.do_lindemann1986
     ild = zeros(nangles,36);
     for ii = 1:nangles
         % generate noise coming from the given direction
-        ir = get_ir(irs,irs.apparent_azimuth(ii));
-        sig = auralize_ir(ir,sig_noise);
+        ir = get_ir(irs,[irs.apparent_azimuth(ii) 0 irs.distance],conf);
+        sig = auralize_ir(ir,sig_noise,1,conf);
         % Ten fold upsampling to have a smoother output
         %sig = resample(sig,10*fs,fs);
         % calculate binaural parameters
@@ -115,10 +119,16 @@ elseif flags.do_lindemann1986
 end
 
 % Fit the lookup data
-for n = 1:12
+for n = 1:size(itd,2)
     [p(:,n),S{n},MU(:,n)] = polyfit(itd(:,n),irs.apparent_azimuth'./pi*180,12);
+    [p_ild(:,n),S_ild{n},MU_ild(:,n)] = ...
+        polyfit(ild(:,n),irs.apparent_azimuth'./pi*180,12);
 end
 % Create lookup struct
 lookup.p = p;
 lookup.MU = MU;
 lookup.S = S;
+lookup.p_ild = p_ild;
+lookup.MU_ild = MU_ild;
+lookup.S_ild = S_ild;
+
