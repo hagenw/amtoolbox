5919b81447658fede3cbf2831920df7b62675e4e
baumsource <baumsource@gmail.com>
2014-03-05 15:41:03 +0100

    data_middlebrooks1999: minor data corrections

--- humandata/data_middlebrooks1999.m
+++ humandata/data_middlebrooks1999.m
@@ -26,18 +26,18 @@ function data = data_middlebrooks1999
 
 % Quantiles: {0,5,25,50,75,95,100}%
   % QE
-  data.qe_own.quantiles = [0,0,1,3,5,13,17];
-  data.qe_own.mean = 3;
-  data.qe_other.quantiles = [7.5,7.5,13,19,28,38,39];
+  data.qe_own.quantiles = [0,0,1,3.5,5,13,17];
+  data.qe_own.mean = 4.5;
+  data.qe_other.quantiles = [7.5,8,12.5,19,27.5,38,39];
   data.qe_other.mean = 21;
   % PE
-  data.pe_own.quantiles = [22,23,25,27,30,34,36];
+  data.pe_own.quantiles = [21,23,25,27,30,34,36];
   data.pe_own.mean = 28;
   data.pe_other.quantiles = [23,33,38,42,48,54,55];
-  data.pe_other.mean = 43;
+  data.pe_other.mean = 42.5;
   % EB
   data.pb_own.quantiles = [1,2.5,6,10,13,20,25.5];
   data.pb_own.mean = 10;
-  data.pb_other.quantiles = [0.5,2,6.5,18,29,42,52];
+  data.pb_other.quantiles = [0.5,2,7,18,29,42,52.5];
   data.pb_other.mean = 19;
 end
