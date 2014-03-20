f6dbfc44d78be02fba95b2e935f44db003aa941e
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-03-20 11:01:51 +0100

    Fixed HRTF handling in wierstorf2013()

--- binaural/wierstorf2013.m
+++ binaural/wierstorf2013.m
@@ -1,41 +1,36 @@
 function [localization_error,perceived_direction,desired_direction,x,y,x0] = ...
         wierstorf2013(X,Y,phi,xs,src,L,varargin);
 %WIERSTORF2013 estimate the localization within a WFS or stereo setup
-%   Usage: [...] = wierstorf2013(X,Y,phi,xs,src,L,resolution,method,...);
+%   Usage: [...] = wierstorf2013(X,Y,phi,xs,src,L,method,...);
 %
 %   Input parameters:
-%       X           : range of the x-axis [xmin,xmax] in m (for a single x point
-%                     you have to specify [x x]
-%       Y           : range of the y-axis [ymin ymax] in m (for a single y point
-%                     you have to specify [y y]
+%       X           : range of the x-axis [xmin,xmax] or a single point x / m
+%       Y           : range of the y-axis [ymin ymax] or a single point y / m
 %       phi         : orientation of the listener in rad (0 is in the direction
-%                     of the x-axis
+%                     of the x-axis)
 %       xs          : position of the point source in m / direction of the 
 %                     plane wave
 %       src         : source type
 %                       'ps' for a point source
 %                       'pw' for a plane wave
 %       L           : length/diameter of the loudspeaker array
-%       resolution  : resolution x resolution is the number of points the
-%                     localization should be estimated in the listening area.
-%                     The points are evenly distributed along the axes. 
 %       method      : reproduction setup
 %                       'wfs' for wave field synthesis
 %                       'setreo' for stereophony
 %
 %   Output parameters:
 %       localization_error  : deviation from the desired direction, defined as
-%                             perceived_direction - desired_direction / rad
+%                             perceived_direction - desired_direction / degree
 %       perceived_direction : the direction of arrival the binaural model has
-%                             estimated for our given setup / rad
+%                             estimated for our given setup / degree
 %       desired_direction   : the desired direction of arrival indicated by the
-%                             source position xs / rad
+%                             source position xs / degree
 %       x                   : corresponding x-axis
 %       y                   : corresponding y-axis
 %       x0                  : position and directions of the loudspeakers in the
-%                             form n x 6, where n is the number of loudspeakers
+%                             form n x 7, where n is the number of loudspeakers
 %
-%   `wierstorf2013(X,Y,phi,xs,src,'wfs',L,nls,array)` calculates the localization
+%   `wierstorf2013(X,Y,phi,xs,src,L,method)` calculates the localization
 %   error for the defined wave field synthesis or stereophony setup. The
 %   localization error is defined here as the difference between the perceived
 %   direction as predicted by the dietz2011 binaural model and the desired
@@ -77,7 +72,7 @@ function [localization_error,perceived_direction,desired_direction,x,y,x0] = ...
 %   revision used to generate the figures in the corresponding paper is
 %   a8914700a4.
 %
-%   See also: estimate_azimuth, dietz2011
+%   See also: wierstorf2013estimateazimuth, dietz2011, itd2angle
 %
 %   References: wierstorf2013 wierstorf2011hrtf dietz2011auditory
 
@@ -109,7 +104,7 @@ hrtf = kv.hrtf;
 lookup = kv.lookup;
 showprogress = kv.showprogress;
 
-% Checking for the Sound-Field-Synthesis Toolbox
+% Check for the Sound-Field-Synthesis Toolbox
 if ~which('SFS_start')
     error(['%s: you need to install the Sound-Field-Synthesis Toolbox.\n', ...
         'You can download it at https://github.com/sfstoolbox/sfs.\n', ...
@@ -117,58 +112,34 @@ if ~which('SFS_start')
         upper(mfilename));
 end
 
-% Checking if we have only one position or if we have a whole listening area
+% Check if we have only one position or if we have a whole listening area
 if length(X)==1 && length(Y)==1
     resolution = 1;
-    X = [X X];
-    Y = [Y Y];
-elseif length(X)==1
-    X = [X X];
-elseif length(Y)==1
-    Y = [Y Y];
 end
 
 
 %% ===== Configuration ===================================================
-% The following settings are all for the Sound Field Synthesis-Toolbox
-% Binaural settings
-% length of impulse response; this has two influences: 
-% 1) longer impulse responses lead to a longer running time of the model
-% 2) shorter impulse responses will not work if your WFS setup needs really long
-% time shifting of single driving signals or you use HRTF with room reflections
-% that rest longer than conf.N samples.
+% The following settings are all for the Sound Field Synthesis Toolbox
 conf.N = 1024;
-% Use no headphone compensation because we are not trying to listening to the
-% signal
 conf.ir.usehcomp = false;
 conf.ir.hcompfile = '';
 conf.ir.useinterpolation = true;
-conf.ir.speechfile = '';
-conf.ir.cellofile = '';
-conf.ir.castanetsfile = '';
-conf.ir.noisefile = '';
-conf.ir.pinknoisefile = '';
-%
-% WFS settings
-% dimensionality of the setup
+conf.ir.useoriglength = false;
 conf.dimension = '2.5D';
-% driving functions
 conf.driving_functions = 'default';
-% Use a WFS pre-equalization filter and specify its start frequency.
-% Note, that the stop frequency will be calculated later with the aliasing
-% frequency of your WFS setup.
 conf.wfs.usehpre = true;
 conf.wfs.hpretype = 'FIR';
 conf.wfs.hpreflow = 50;
-% Tapering window
 conf.usetapwin = true;
 conf.tapwinlen = 0.3;
-% misc settings
-conf.debug = 0;
+conf.debug = false;
+conf.showprogress = false;
 conf.c = 343;
 conf.fs = 44100;
 conf.usefracdelay = 0;
 conf.fracdelay_method = '';
+conf.secondary_sources.center = [0 0 0];
+conf.secondary_sources.x0 = [];
 
 
 %% ===== Loading of additional data ======================================
@@ -178,8 +149,7 @@ if isempty(hrtf)
     % load HRTFs, see:
     % https://dev.qu.tu-berlin.de/projects/measurements/wiki/2010-11-kemar-anechoic
     [~,path] = download_hrtf('wierstorf2011_3m');
-    load([path 'wierstorf2011_3m.mat']);
-    hrtf = irs;
+    hrtf = read_irs([path 'wierstorf2011_3m.mat'],conf);
 end
 % Get sampling rate from the HRTFs
 fs = hrtf.fs;
@@ -193,7 +163,7 @@ end
 
 
 %% ===== Simulate the binaural ear signals ===============================
-% Simulate a stereo setup
+% array geometry
 conf.secondary_sources.geometry = array;
 % center of array
 conf.secondary_sources.center = [0 0 0];
@@ -205,26 +175,25 @@ conf.secondary_sources.number = number_of_speakers;
 conf.secondary_sources.size = L;
 % get loudspeaker positions
 x0 = secondary_source_positions(conf);
+% calculate the stop frequency for the WFS pre-equalization filter
+conf.wfs.hprefhigh = aliasing_frequency(x0,conf);
+
 % selection of loudspeakers for WFS
 if flags.do_wfs && strcmpi('circle',array)
     x0 = secondary_source_selection(x0,xs,src);
 end
 
-% calculate the stop frequency for the WFS pre-equalization filter
-conf.wfs.hprefhigh = aliasing_frequency(x0,conf);
-
 % get a grid of the listening positions
 conf.resolution = resolution;
-[~,~,x,y] = xy_grid(X,Y,conf);
-% simulate the binaural impulse response
-perceived_direction = zeros(length(x),length(y));
-desired_direction = zeros(length(x),length(y));
-localization_error = zeros(length(x),length(y));
+[~,~,~,x,y] = xyz_grid(X,Y,0,conf);
+
+% 700 ms white noise burst
+sig_noise = whitenoiseburst(fs);
 for ii=1:length(x)
-    if showprogress progressbar(ii,length(x)) end
-    parfor jj=1:length(y)
+    progressbar(ii,length(x))
+    for jj=1:length(y)
         X = [x(ii) y(jj) 0];
-        conf.xref = X;
+        desired_direction(ii,jj) = source_direction(X,phi,[xs 0],src);
         if flags.do_stereo
             % first loudspeaker
             ir1 = ir_point_source(X,phi,x0(1,1:3),hrtf,conf);
@@ -233,27 +202,17 @@ for ii=1:length(x)
             % sum of both loudspeakers
             ir = (ir1+ir2)/2;
         else % WFS
-            ir = ir_wfs(X,phi,[xs 0],src,hrtf,conf);
+            conf.xref = X;
+            ir = ir_wfs(X,pi/2,[xs 0],src,hrtf,conf);
         end
-        % generate a 0.1s noise signal
-        sig_noise = noise(fs/10,1,'white');
-        % convolve with impulse response
+        % convolve impulse response with noise burst
         sig = auralize_ir(ir,sig_noise,1,conf);
-
-
-        %% ===== Estimate the direction of arrival for the listener ==============
-        % this is done by calculating ITDs with the dietz2011 binaural model, which are
-        % then mapped to azimuth values with a lookup table
-        %
-        % estimate the perceived direction of arrival
-        perceived_direction(ii,jj) = ...
-            estimate_azimuth(sig,lookup,'dietz2011','no_spectral_weighting','remove_outlier');
-        % calculate the desired direction
-        desired_direction(ii,jj) = source_direction(X,phi,[xs 0],src);
-        % calculate the localization error as the difference of both
+        % estimate the perceived direction
+        perceived_direction(ii,jj) = wierstorf2013estimateazimuth(sig,lookup, ...
+            'dietz2011','no_spectral_weighting','remove_outlier');
+        localization_error(ii,jj) = desired_direction(ii,jj)-perceived_direction(ii,jj);
     end
 end
-localization_error = perceived_direction - desired_direction;
 
 end % of main function
 
@@ -261,13 +220,11 @@ end % of main function
 %% ----- Subfunctions ----------------------------------------------------
 function direction = source_direction(X,phi,xs,src)
     if strcmp('pw',src)
-        [direction,~,~] = cart2sph(xs(1),xs(2),0);
-        direction = deg(direction+phi);
+        [direction,~,~] = cart2sph(xs(1),xs(2),xs(3));
+        direction = (direction+phi)/pi*180;
     elseif strcmp('ps',src)
         x = xs-X;
-        [direction,~,~] = cart2sph(x(1),x(2),0);
-        % FIXME: this is not working with all points at the moment
-        % For example place a stereo source at (0,0) and the listener at (0,-2)
-        direction = deg(direction-phi);
+        [direction,~,~] = cart2sph(x(1),x(2),x(3));
+        direction = (direction-phi)/pi*180;
     end
 end
