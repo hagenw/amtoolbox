502f07aa3ccd6710f2f99374c8ed0a2fd6c5158e
petibub <piotr@majdak.com>
2014-05-22 22:20:10 +0200

    baumgartner2014likelistat.m: convert to unicode

--- modelstages/baumgartner2014likelistat.m
+++ modelstages/baumgartner2014likelistat.m
@@ -29,7 +29,7 @@ definput.flags.normlikeli = {'normalize','original'};
 definput.flags.plot = {'noplot','plot'};
 definput.flags.jackknife = {'','jackknife'};
 
-definput.keyvals.runs = 100;  
+definput.keyvals.runs = 100;   
 
 [flags,kv]=ltfatarghelper({'runs'},definput,varargin);
 
