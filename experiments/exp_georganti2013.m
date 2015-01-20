923f44228843488e3d5464288e45a8a3f01f2cc5
petibub <piotr@majdak.com>
2015-01-20 22:36:55 +0100

    work on georganti2013 started. Original file from the author.

--- experiments/exp_georganti2013.m
+++ experiments/exp_georganti2013.m
@@ -1,42 +1,113 @@
-% Matlab code for computation of the distance-dependent feature
-% BSMD STD (Binaural Spectral Magnitude Difference Standard Deviation)
-%
-% The feature can be derived from any dual-channel signal
-% (binaural/stereo recordings).
-%
+function varargout = exp_georganti2013(flag)
 
-% More information can be found in :
-% [1] E. Georganti, T. May, S. van de Par, and J. Mourjopoulos. "Extracting
-%     sound-source-distance information from binaural signals." In
-%     J. Blauert, editor, The technology of binaural listening, chapter 7.
-%     Springer, Berlin?Heidelberg?New York NY, 2013.
+%EXP_GEORGANTI2013 Figures from Georganti et al. (2013)
+% 
+%
+%   EXP_GEORGANTI2013(FLAG) reproduces figures that can be found in  
+%   Georganti et al. (2013) [1].
+%   
+%   FLAG maybe either 'fig9' or 'fig10'
+%
+%   The figures present the computed distance-dependent feature
+%   BSMD STD (Binaural Spectral Magnitude Difference Standard Deviation)
+%   for audio signals measured at different/receiver distances.
+%
+%   The BSMD STD may be derived from any dual-channel signal
+%   (binaural/stereo recordings).
+%
+%   The BSMD STD feature is related to the standard deviation of the
+%   magnitude spectrum of Room Transfer Function, which is known to depend on 
+%   the source/receiver distance. See [2] for more information.
+%
+%
+%   REFERENCES:
+%
+%   [1] E. Georganti, T. May, S. van de Par, and J. Mourjopoulos. "Extracting
+%   sound-source-distance information from binaural signals." In
+%   J. Blauert, editor, The technology of binaural listening, chapter 7.
+%   Springer, Berlin, Heidelberg, New York NY, 2013.
+%   url: http://link.springer.com/chapter/10.1007%2F978-3-642-37762-4_7
+%
+%   [2] E. Georganti, T. May, S. van de Par, and J. Mourjopoulos, "Sound
+%   Source Distance Estimation in Rooms based on Statistical Properties of
+%   Binaural Signals", IEEE Transactions on Audio, Speech and Language 
+%   Processing, Vol. 21 (8), Aug. 2013.
+%   url: http://ieeexplore.ieee.org/xpl/articleDetails.jsp?arnumber=6508870
+%
+%   ---------
+%
+%   REQUIREMENTS: 
+%
+%   1) Download audio files from 
+%      http://sourceforge.net/projects/audiofordistanceestimation/files/wavFilesGeorganti.zip/download
+%      (copy/paste the above link at your browser)
+%
+%   2) Unzip folder (wavFilesGeorganti.zip)
+%
+%   3) Move folder in amtoolbox/signals/
+%
+%
+%   ---------
+%
+%   EXAMPLE:
+%
+%   To display Fig.9 [1] use :
+%
+%     exp_georganti2013('fig9');
+%
+%   To display Fig.10 [1] use :
+%
+%     exp_georganti2013('fig10');
+%
+%
+%   Url: http://amtoolbox.sourceforge.net/doc/experiments/exp_georganti2013.php
 
+%   Copyright (C) 2009-2014 Peter L. S??ndergaard and Piotr Majdak.
+%   This file is part of AMToolbox version 0.9.5
 %
-%   This is a very early version of the code. Updates are on their way!
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %
 %
-%   Developed with Matlab 7.1.0.584 (R2010b). Please send bug reports to:
+%
+%   Developed with Matlab 7.1.0.584 (R2010b)      v.0.1
+%   Updated   with Matlab R2011b    (7.13.0.564)  v.1.0
+%
+%   Please send bug reports to:
 %
 %   Author  :  Eleftheria Georganti
-%              Phd student
-%              Audio And Acoustic Technology Laboratoty
-%              University of Patras, Rion, Greece
-%              egeorganti@upatras.gr
+%              Postdoctoral Researcher
+%              Experimental Audiology, ENT
+%              University Hospital of Zurich/University of Zurich
+%              Zurich, Switzerland
+%              eleftheria.georganti@uzh.ch
 
 %   History :
 %   v.0.1   2013/01/21
-
-% *************************************************************************
-
-clc
-close all
-clear all
+%   v.1.0   2014/06/10 Link to wavfiles & documentation
+% 
+%
+%*************************************************************************
 
 
 
 % SELECT SMALL OR LARGE ROOM BY COMMENTING/UNCOMMENTING
- num = 1      % Small Room -  RT = 0.15 sec
-% num = 2      % Large Room -  RT = 0.9 sec
+
+if flag =='fig09'
+    num = 1;      % Small Room -  RT = 0.15 sec
+elseif flag == 'fig10'
+    num = 2;      % Large Room -  RT = 0.9 sec
+end
 
 
 
@@ -58,11 +129,11 @@ P.hop = P.sampleFr/2;
 P.nFFT = P.sampleFr;
 
 % Frequency index
-P.freq = (P.fs/P.nFFT)*(0:(P.nFFT/2-1))
+P.freq = (P.fs/P.nFFT)*(0:(P.nFFT/2-1));
 
 % Define frequency range for the BSDM STD feature calculation
-P.fmin = 20;    % lower frequency in Hz
-P.fmax = 2300; % upper frequency in Hz
+P.fmin = 20;    % lower frequency in Hz - default value
+P.fmax = 2300; % upper frequency in Hz - default value
 
 
 % List the number of available distances for the wavfiles
@@ -70,10 +141,9 @@ P.fmax = 2300; % upper frequency in Hz
 
 
 if num == 1
-    nDist = listDirs(['wavFiles',filesep,'smallRoom']);  
+    nDist = listDirs(['signals',filesep,'wavFilesGeorganti',filesep,'smallRoom']);  
 elseif num == 2;
-    nDist = listDirs(['wavFiles',filesep,'largeRoom']);   
-    
+    nDist = listDirs(['signals',filesep,'wavFilesGeorganti',filesep,'largeRoom']);   
 end
 
 
@@ -85,8 +155,9 @@ idx = 1;
 
 for ww = 1:length(nDist)
     
-    pathWav = listFiles(nDist(ww).name,'*.wav');
-    signal = wavread(pathWav(1).name);
+    pathWav = listFiles(nDist(ww).name,'*.mat');
+    signalPre = load(pathWav(1).name);
+    signal = signalPre.signal;
     
     for kk = 1:P.hop:length(signal)-P.hop
         
@@ -124,6 +195,7 @@ if num == 1
     plot(difSTD(3,10:210),'--k','LineWidth',2)
     xlabel('Time (sec)','FontSize',16,'FontWeight','bold')
     ylabel('BSMD STD','FontSize',16,'FontWeight','bold')
+    title ('Small Room -  RT = 0.15 sec','FontSize',16,'FontWeight','bold')
     ylim([3 8])
     xlim([0 200])
     legend('0.5m','1m','1.5m','Location','NorthWest')
@@ -161,6 +233,7 @@ elseif num == 2
     plot(difSTD(3,10:210),'--k','LineWidth',2)
     xlabel('Time (sec)','FontSize',16,'FontWeight','bold')
     ylabel('BSMD STD','FontSize',16,'FontWeight','bold')
+    title ('Large Room -  RT = 0.9 sec','FontSize',16,'FontWeight','bold')
     ylim([2 8.5])
     xlim([0 200])
     legend('1m','2m','3m','Location','NorthWest')
@@ -187,4 +260,7 @@ elseif num == 2
     
     print -depsc2 largeSpeechBSMD_STD  %Creates .eps file with the figure
 end
+
+
+
 
