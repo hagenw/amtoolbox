4c526d7a52467d7ca4a1b575dbe6ecc67219b2fb
petibub <piotr@majdak.com>
2015-01-29 10:45:15 +0100

    reference directory removed (it was a directory with original contributions to the AMT)

--- reference/Contents.m
+++
@@ -1,6 +0,0 @@
-% AMT Reference - Reference code to evaluate the AMT
-%
-%  For help, bug reports, suggestions etc. please send email to
-%  amtoolbox-help@lists.sourceforge.net
-
-
--- reference/culling/f_of_erb_rate.m
+++
@@ -1,3 +0,0 @@
-function f = f_of_erb_rate(erb)
-f = (0.312 - (exp((erb - 43)/11.17)) * 14.675) / (exp((erb - 43)/11.17) - 1);
-f = f * 1000; %convert f to Hz not kHz
