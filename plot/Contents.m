bc1f347dc487e9ec7dcd545c9e8f6796c5c832e9
petibub <piotr@majdak.com>
2015-06-01 23:45:45 +0200

    docu bug fix

--- plot/Contents.m
+++ plot/Contents.m
@@ -7,11 +7,11 @@
 %     PLOT_BAUMGARTNER2013 - plot probabilistic prediction matrixes
 %     PLOT_BAUMGARTNER2014 - plot probabilistic prediction matrixes
 %     PLOT_BAUMGARTNER2014LIKELISTAT - Likelihood statistics in the model
-%     PLOT_JOERGENSEN2011
-%     PLOT_JOERGENSEN2013
+%     PLOT_JOERGENSEN2011 - Plot output of Joergensen2011 model
+%     PLOT_JOERGENSEN2013 - Plot output of Joergensen2013 model
 %     PLOT_LANGENDIJK2002 - Plot the pdf-matrices with grey colormap
 %     PLOT_LANGENDIJK2002LIKELIHOOD - Plot the likelihood statistics
-%     PLOT_LINDEMANN1986
+%     PLOT_LINDEMANN1986 - plot of lindemann1986 model
 %     PLOT_ROENNE2012 - Plot output of roenne2012
 %     PLOT_ROENNE2012CHIRP - Plot output of roenne2012chirp
 %     PLOT_ROENNE2012TONEBURSTS - Plot output of roenne2012tonebursts
