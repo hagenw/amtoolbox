e68d6917d1017bd6e9479cbf8db31fe4221880ba
petibub <piotr@majdak.com>
2016-01-15 20:25:22 +0100

    models/Contents updated

---
+++ models/Contents.m
@@ -0,0 +1,37 @@
+% AMT - Publication-specific models
+%
+%  The AMT team, 2012 - 2016.
+%
+%  HRTF models
+%     ENZNER2008            - Azimuth-continuous measurement of HRTFs 
+%     ZIEGELWANGER2014      - Direction-continuous model of time-of-arrival (TOA) in HRTFs
+%     ZIEGELWANGER2013      - Direction-continuous model of time-of-arrival (TOA) in HRTFs (depracated)
+%
+%  Peripheral models
+%     HOHMANN2002           - Invertible Gammatone filterbank 
+%     VIEMEISTER1979        - Modulation detection model
+%     VERHULST2012          - Cochlear transmition-line model 
+%     ZILANY2007HUMANIZED   - Humanized auditory nerve model
+%     ZILANY2014            - Auditory cochlear processing 
+%     ROENNE2012            - Simulate ABR to given stimulus
+%
+%  Spatial models
+%     BAUMGARTNER2013       - Localization in saggital planes
+%     BAUMGARTNER2014       - Localization in saggital planes
+%     LINDEMANN1986         - Calculate a binaural activity map
+%     DIETZ2011             - Binaural azimuth estimation 
+%     WIERSTORF2013         - Localization in a WFS or stereo setup
+%     TAKANEN2013           - Binaural count-comparison model
+%     MAY2011               - Azimuth of concurrent speakers
+%     GEORGANTI2013         - Distance estimation
+%     LANGENDIJK2002        - Median-plane localization probability
+%
+%  Speech perception models
+%    TAAL2011               - Short-time objective intelligibility
+%    JOERGENSEN2011         - Speech-based envelope power spectrum based on Joergensen et al. (2011)
+%    JOERGENSEN2013         - Speech-based envelope power spectrum based on Joergensen et al. (2013)
+%    JELFS2011              - Binaural speech advantage
+
+%
+%  For help, bug reports, suggestions etc. please send email to
+%  amtoolbox-help@lists.sourceforge.net
