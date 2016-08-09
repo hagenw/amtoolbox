bbb3aea6118256e761259a966bcaba5bde412332
Martina <m.kreuzbichler@gmail.com>
2016-08-09 15:51:13 +0200

    new exp_breebaart2001 computation

---
+++ experiments/exp_breebaart2001.m
@@ -0,0 +1,260 @@
+function output=exp_breebaart2001(varargin)
+%EXP_BREEBAART2001   Figures from Breebaart et al.(2001a and 2001b)
+%   Usage: output = exp_breebaart2001(flag)
+%
+%   `exp_breebaart2001(flags,... )` reproduces experiments from the 
+%         Breebaart et al (2001a and 2001b) papers.
+%
+%   The following flags can be specified;
+%
+%     'plot'    Plot the output of the experiment. This is the default.
+%
+%     'noplot'  Don't plot, only return data.
+%
+%     'afig2'    Reproduce Fig. 2 from Breebaart et al. (2001a)
+%
+%
+%   See also: data_breebaart2001 and data_vandepar1999
+%
+%   Examples:
+%   ---------
+%
+%   To display Figure 2 of the 2001a paper use :::
+%
+%     exp_breebaart2001('afig2');
+%
+
+%
+%   References:
+  
+%  AUTHOR: Martina Kreuzbichler
+
+%% ------ Check input options --------------------------------------------
+
+  definput.import={'amtcache'};
+  definput.flags.type = {'missingflag','afig2','afig6','bfig3','bfig6',...
+      'fig1_N0S0_vandepar1999'};
+  definput.flags.plot = {'plot','noplot'};
+
+  % Parse input options
+  [flags,kv]  = ltfatarghelper({},definput,varargin);
+        
+if flags.do_missingflag
+  flagnames=[sprintf('%s, ',definput.flags.type{2:end-2}),...
+             sprintf('%s or %s',definput.flags.type{end-1},definput.flags.type{end})];
+  error('%s: You must specify one of the following flags: %s.',upper(mfilename),flagnames);
+end;
+
+%% Breebaart et al. (2001a) fig. 2
+if flags.do_afig2
+    
+    testsigadlo = amtcache('get','afig2',flags.cachemode);
+    
+    if isempty(testsigadlo)
+        definput.import = {'auditoryfilterbank','ihcenvelope','adaptloop','eicell'};
+        definput.importdefaults={'fhigh',8000,'ihc_breebaart','adt_breebaart'};
+
+        [flags,keyvals,flow,fhigh,basef]  = ltfatarghelper({'flow', 'fhigh', ...
+                            'basef'},definput,varargin);
+
+
+        testsig(:,1) = sin(2*pi*(0:(0.1*48000)-1)'*500/48000);
+        testsig(:,2) = sin(2*pi*(0:(0.1*48000)-1)'*4000/48000);
+
+        testsig = setdbspl(testsig,70);
+        testsig = [testsig; zeros(0.1*48000,2)];
+
+        for counter = 1:2
+
+            testsigoutmiddle(:,counter) = outmiddleartransfunct(testsig(:,counter),48000);
+
+            [testsigaudfilt(:,:,counter), fc] = auditoryfilterbank(testsigoutmiddle(:,counter),48000,'argimport',flags,keyvals);
+
+            testsigihc(:,:,counter) = ihcenvelope(testsigaudfilt(:,:,counter),48000,'argimport',flags,keyvals);
+
+            testsigadlo(:,:,counter) = adaptloop(testsigihc(:,:,counter),48000,'argimport',flags,keyvals);
+        end
+        
+        amtcache('set','afig2',testsigadlo)
+        
+    end
+    
+    output = testsigadlo;
+    
+% elseif flags.do_afig6
+%     definput.import = {'auditoryfilterbank','ihcenvelope','adaptloop','eicell'};
+%     definput.importdefaults={'fhigh',8000,'ihc_breebaart','adt_breebaart'};
+% 
+%     [flags,keyvals,flow,fhigh,basef]  = ltfatarghelper({'flow', 'fhigh', ...
+%                         'basef'},definput,varargin);
+%                     
+%     testsig(:,1) = bandpassnoisefreq(2000,4800, par.ndur,par.nl,par.nbw);
+%     
+%     testsigoutmiddle(:,counter) = outmiddleartransfunct(testsig(:,counter),48000);
+%     
+%     [testsigaudfilt(:,:,counter), ~] = auditoryfilterbank(testsigoutmiddle(:,counter),48000,'argimport',flags,keyvals);
+%     
+%     testsigihc(:,:,counter) = ihcenvelope(testsigaudfilt(:,:,counter),48000,'argimport',flags,keyvals);
+% 
+%     testsigadlo(:,:,counter) = adaptloop(testsigihc(:,:,counter),48000,'argimport',flags,keyvals);
+% 
+%     %[siglen,nfreqchannels,naudiochannels,nsignals] = size(testsigadlo);
+%     %ei_map = zeros(siglen, nfreqchannels, nsignals);
+% 
+%     taucount = 1;
+%     ildcount = 1;
+%     for tau = -0.002:0.0001:0.002
+%         for ild = -10:0.5:10
+%             ei_map_(taucount,ildcount,:) = eicell(squeeze(testsigadlo(:,9,:,1)),fs,tau,ild); % 9 ist Filter bei 500 Hz
+%             ildcount = ildcount + 1;
+%         end
+%         ildcount = 1;
+%         taucount = taucount + 1;
+%     end
+
+
+elseif flags.do_fig1_N0S0_vandepar1999
+    
+    [N0S0125,N0S0250,N0S0500,N0S01000,N0S02000,N0S04000] = ...
+        amtcache('get','fig1_N0S0_vandepar1999',flags.cachemode);
+    
+    if isempty(N0S0125)
+%         RUN EXPERIMENT
+    end
+
+    output = struct('N0S0125',N0S0125,'N0S0250',N0S0250,'N0S0500',N0S0500,...
+        'N0S01000',N0S01000,'N0S02000',N0S02000,'N0S040000',N0S04000);  
+end
+
+
+if flags.do_plot
+    if flags.do_afig2
+        fax = 0:0.2/9599:0.2;
+        hafig2 = figure;
+        set(hafig2, 'Position', [400 400 1200 450])
+        subplot(1,2,1)
+        plot(fax,output(:,9,1))
+        axis([0 0.2 -1000 8500])
+        xlabel('time [s]')
+        ylabel('Output [MU')
+        text('Position',[0.1 7000],'string','500 Hz tone')
+        subplot(1,2,2)
+        plot(fax,output(:,25,2))
+        axis([0 0.2 -1000 8500])
+        xlabel('time [s]')
+        ylabel('Output [MU]')
+        text('Position',[0.1 7000],'string','4000 Hz tone')
+        suptitle('Fig. 2 Output of the peripheral preprocessor')
+        
+    elseif flags.do_fig1_N0S0_vandepar1999
+        
+        %get experimental data
+        N0S0expdata125 = data_vandepar1999('fig1_N0S0','nfc125');
+        N0S0expdata250 = data_vandepar1999('fig1_N0S0','nfc250');
+        N0S0expdata500 = data_vandepar1999('fig1_N0S0','nfc500');
+        N0S0expdata1000 = data_vandepar1999('fig1_N0S0','nfc1000');
+        N0S0expdata2000 = data_vandepar1999('fig1_N0S0','nfc2000');
+        N0S0expdata4000 = data_vandepar1999('fig1_N0S0','nfc4000');
+        
+        
+        
+        
+        hfig1=figure;
+        set(hfig1, 'Position', [100 100 1100 700])
+        
+        bw = [5 10 25 50 100 250];
+        s1 = subplot(3,2,1);
+        p1 = get(s1,'pos');
+        errorbar(bw,N0S0125(:,1),N0S0125(:,2),'-x','MarkerSize',10);
+        set(gca,'xscal','log')
+        axis([4 5000 -30 10])
+        hold on;
+        plot(bw,N0S0expdata125,'-og')
+        text('Position',[1000 5],'string','125 Hz')
+        ylabel('Threshold S/N [dB]');
+
+        bw = [5 10 25 50 100 250 500];
+        s2 = subplot(3,2,2);
+        p2 = get(s2,'pos');
+        p2(1) = p1(1) + p1(3);
+        set(s2, 'pos', p2);
+        errorbar(bw,N0S0250(:,1),N0S0250(:,2),'-x','MarkerSize',10);
+        set(gca,'xscal','log')
+        axis([4 5000 -30 10])
+        set(gca,'XTick',[]);
+        set(gca,'YTick',[]);
+        hold on;
+        plot(bw,N0S0expdata250,'-og')
+        text('Position',[1000 5],'string','250 Hz')
+
+        bw = [5 10 25 50 100 250 500 1000];
+        s3 = subplot(3,2,3);
+        p3 = get(s3,'pos');
+        p3(2) = p1(2) - p1(4) + 0.005;
+        set(s3, 'pos', p3);
+        errorbar(bw,N0S0500(:,1),N0S0500(:,2),'-x','MarkerSize',10);
+        set(gca,'xscal','log')
+        axis([1 10000 -30 10])
+        set(gca,'XTick',[]);
+        set(gca,'YTick',[-30 -20 -10 0]);
+        hold on;
+        plot(bw,N0S0expdata500,'-og')
+        text('Position',[1000 5],'string','500 Hz')
+        ylabel('Threshold S/N [dB]');
+
+        bw = [5 10 25 50 100 250 500 1000 2000];
+        s4 = subplot(3,2,4);
+        p4 = get(s4,'pos');
+        p4(1) = p3(1) + p3(3);
+        p4(2) = p3(2);
+        set(s4, 'pos', p4);
+        errorbar(bw,N0S01000(:,1),N0S01000(:,2),'-x','MarkerSize',10);
+        set(gca,'xscal','log')
+        axis([1 10000 -30 10])
+        set(gca,'XTick',[]);
+        set(gca,'YTick',[]);
+        hold on;
+        plot(bw,N0S0expdata1000,'-og')
+        text('Position',[1000 5],'string','1000 Hz')
+        xlabel('Bandwidth [Hz]');
+
+        bw = [5 10 25 50 100 250 500 1000 2000 4000];
+        s5 = subplot(3,2,5);
+        p5 = get(s5,'pos');
+        p5(2) = p3(2) - p3(4) + 0.005;
+        set(s5, 'pos', p5);
+        errorbar(bw,N0S02000(:,1),N0S02000(:,2),'-x','MarkerSize',10);
+        set(gca,'xscal','log')
+        set(gca,'XTick',[1 10 100 1000 10000]);
+        axis([1 10000 -30 10])
+        set(gca,'YTick',[-30 -20 -10 0]);
+        hold on;
+        plot(bw,N0S0expdata2000,'-og')
+        text('Position',[1000 5],'string','2000 Hz')
+        xlabel('Bandwidth [Hz]');
+        ylabel('Threshold S/N [dB]');
+
+        s6 = subplot(3,2,6);
+        p6 = get(s6,'pos');
+        p6(1) = p5(1) + p5(3);
+        p6(2) = p5(2);
+        set(s6, 'pos', p6);
+        errorbar(bw,N0S04000(:,1),N0S04000(:,2),'-x','MarkerSize',10);
+        set(gca,'xscal','log')
+        set(gca,'XTick',[ 10 100 1000 10000]);
+        axis([1 10000 -30 10])
+        set(gca,'YTick',[]);
+        hold on;
+        plot(bw,N0S0expdata4000,'-og')
+        text('Position',[1000 5],'string','4000 Hz')
+        xlabel('Bandwidth [Hz]');
+        
+        legend({'modeled data SSL = 90 dB, NL 70 dB  - monaural factor = 0.0003',...
+            'experimental data from van de Paar and Kohlrausch (1999)'},...
+            'Position' ,[p2(1)-p2(3)*0.2,0.15,0.1,0.05]);
+        
+        suptitle('Fig. 1 N_0S_0 Thresholds')
+    end
+end
+    
+    
